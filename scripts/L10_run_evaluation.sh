e3fec5d06f078f56d4969ffe57abe775d9ba2b71
MotacillaAlba <rowena.lily.lee@gmail.com>
2024-12-17 11:34:58 +0800

    🔇

--- scripts/L10_run_evaluation.sh
+++ scripts/L10_run_evaluation.sh
@@ -20,27 +20,27 @@ export DEBUG_CHALLENGE=0
 
 export SCENARIOS=${LEADERBOARD_ROOT}/data/all_towns_traffic_scenarios_public.json
 export ROUTES=${LEADERBOARD_ROOT}/data/routes_testing.xml
-export ROUTES_SUBSET=3
+export ROUTES_SUBSET=0
 export REPETITIONS=1
 export CHALLENGE_TRACK_CODENAME=SENSORS
 export TIME_STAMP=$(date +"%s")
-export CHECKPOINT_ENDPOINT=${WORKSPACE}/logs/L10_testing/route_${ROUTES_SUBSET}_${TIME_STAMP}.json
+export CHECKPOINT_ENDPOINT=${WORKSPACE}/logs/L10_testing/route_${ROUTES_SUBSET}_seed_${SEED}_${TIME_STAMP}.json
 export TEAM_AGENT=${WORKSPACE}/team_code/lav_agent.py
 export TEAM_CONFIG=${WORKSPACE}/team_code/config.yaml
 
 RESUME=0
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
---port=${PORT} \
---trafficManagerPort=${TM_PORT} \
---trafficManagerSeed=${SEED} \
---scenarios=${SCENARIOS}  \
---routes=${ROUTES} \
---routes-subset=${ROUTES_SUBSET} \
---repetitions=${REPETITIONS} \
---track=${CHALLENGE_TRACK_CODENAME} \
---checkpoint=${CHECKPOINT_ENDPOINT} \
---agent=${TEAM_AGENT} \
---agent-config=${TEAM_CONFIG} \
---debug=${DEBUG_CHALLENGE} \
---resume=${RESUME}
+    --port=${PORT} \
+    --trafficManagerPort=${TM_PORT} \
+    --trafficManagerSeed=${SEED} \
+    --scenarios=${SCENARIOS}  \
+    --routes=${ROUTES} \
+    --routes-subset=${ROUTES_SUBSET} \
+    --repetitions=${REPETITIONS} \
+    --track=${CHALLENGE_TRACK_CODENAME} \
+    --checkpoint=${CHECKPOINT_ENDPOINT} \
+    --agent=${TEAM_AGENT} \
+    --agent-config=${TEAM_CONFIG} \
+    --debug=${DEBUG_CHALLENGE} \
+    --resume=${RESUME}
